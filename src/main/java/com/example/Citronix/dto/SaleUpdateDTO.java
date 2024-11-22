3ee70b347c502d26f18ea8e03b751b97d7549831
MohammedFytahi <mohammedfytahi567@gmail.com>
2024-11-22 10:08:28 +0100

    CIT-16 sales updating

--- src/main/java/com/example/Citronix/dto/SaleUpdateDTO.java
+++ src/main/java/com/example/Citronix/dto/SaleUpdateDTO.java
@@ -1,25 +1,19 @@
 package com.example.Citronix.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-public class SaleCreateDTO {
+public class SaleUpdateDTO {
 
-    @NotNull(message = "La date de vente est obligatoire.")
     private LocalDate saleDate;
 
-    @NotNull(message = "Le prix unitaire est obligatoire.")
     @Positive(message = "Le prix unitaire doit être un nombre positif.")
     private Double unitPrice;
 
-    @NotBlank(message = "Le nom du client est obligatoire.")
+    @NotBlank(message = "Le nom du client ne peut pas être vide.")
     private String clientName;
-
-    @NotNull(message = "L'ID de la récolte est obligatoire.")
-    private Long harvestId;
 }
